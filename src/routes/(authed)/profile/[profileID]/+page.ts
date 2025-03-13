e34efe510d43a7efeedd37ad14cc8ed997218e20
p4elkab35t <saganiter234@gmail.com>
2025-03-12 18:08:54 -0700

    async fetch fix #1

--- src/routes/(authed)/profile/[profileID]/+page.ts
+++ src/routes/(authed)/profile/[profileID]/+page.ts
@@ -3,13 +3,10 @@ import { SocialAPI } from '$lib/api/social';
 
 export const load: PageLoad = async ({ params }) => {
 	// Fetch profile info from API instead of mockData
-	const profileResponse = await SocialAPI.getProfile('profile', params.profileID);
-	if (profileResponse.error) {
-		return { status: 404, error: new Error(`Profile not found`) };
-	}
+	const profileResponse = SocialAPI.getProfile('profile', params.profileID);
 
 	// Fetch posts by this profile from API
-	const postsResponse = await SocialAPI.getPosts({userId: params.profileID});
+	const postsResponse = SocialAPI.getPosts({userId: params.profileID});
 	return {
 		profile: profileResponse,
 		posts: postsResponse
