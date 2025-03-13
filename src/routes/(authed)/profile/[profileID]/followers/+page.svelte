26b35bd00510cb8e2f1176723e7e6ef15749d38a
p4elkab35t <saganiter234@gmail.com>
2025-03-13 09:42:24 -0700

    fix followercard styles #5

--- src/routes/(authed)/profile/[profileID]/followers/+page.svelte
+++ src/routes/(authed)/profile/[profileID]/followers/+page.svelte
@@ -34,7 +34,7 @@
     <h2 class="text-zinc-100 px-4 lg:px-0 text-2xl lg:text-5xl font-bold -translate-y-4 md:-translate-y-2 lg:-translate-y-4">    
         Followers
     </h2>
-    <div class="flex flex-col gap-4 py-6 bg-zinc-300 px-4">
+    <div class="flex flex-col gap-4 py-6 bg-zinc-300 px-4 w-full">
         {#if followers.length === 0 && !isLoading}
             <p class="text-zinc-100 text-lg">No followers yet</p>
         {:else if isLoading}
--- src/routes/(authed)/profile/[profileID]/following/+page.svelte
+++ src/routes/(authed)/profile/[profileID]/following/+page.svelte
@@ -34,7 +34,7 @@
     <h2 class="text-zinc-100 px-4 lg:px-0 text-2xl lg:text-5xl font-bold -translate-y-4 md:-translate-y-2 lg:-translate-y-4">    
         Following
     </h2>
-    <div class="flex flex-col gap-4 py-6 bg-zinc-300 px-4">
+    <div class="flex flex-col gap-4 py-6 bg-zinc-300 px-4 w-full">
         {#if following.length === 0 && !isLoading}
             <p class="text-zinc-100 text-lg">Not following anyone yet</p>
         {:else if isLoading}
